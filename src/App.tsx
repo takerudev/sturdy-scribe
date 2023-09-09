0b8b5897c50f90e228cd7b6f22d11452907eff15
takerudev <takerudev@protonmail.com>
2023-09-09 17:00:40 +1000

    Handle lorebook upload then display

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import { Container } from "react-bootstrap";
-import lorebookRaw from "./schemas/lorebook-samples/lorebook-test.json";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Form from "react-bootstrap/Form";
+// import lorebookRaw from "./schemas/lorebook-samples/lorebook-test.json";
 import LorebookPanel from "./components/Lorebook";
 import validateLorebook from "./utils/schemaHandler";
+import { useCallback, useEffect, useState } from "react";
+import { Lorebook } from "./types";
 
 const App = () => {
+  // TODO: Use this template for new lorebooks
   // const newEntryTemplate = {
   //   key: [],
   //   keysecondary: [],
@@ -21,10 +26,47 @@ const App = () => {
   //   useProbability: true,
   // };
 
-  const lorebook = validateLorebook(lorebookRaw);
+  const [files, setFiles] = useState<Array<File>>([]);
+  const [lorebook, setLorebook] = useState<Lorebook>();
+
+  const handleUpload = useCallback(
+    async (e: React.ChangeEvent<HTMLInputElement>) => {
+      e.stopPropagation();
+      e.preventDefault();
+      e.persist();
+      for (const file of e.target.files ?? []) {
+        if (file.type === "application/json") setFiles([file]);
+      }
+    },
+    [],
+  );
+
+  const updateLorebook = useCallback(
+    async (file: File) => {
+      console.log("updateLorebook", files);
+      const rawLorebook = JSON.parse(await file.text());
+      setLorebook(validateLorebook(rawLorebook));
+    },
+    [files],
+  );
+
+  useEffect(() => {
+    console.log("useEffect files", files);
+    if (files.length > 0) updateLorebook(files[0]);
+  }, [files, updateLorebook]);
 
   return (
-    <Container>{lorebook && <LorebookPanel lorebook={lorebook} />}</Container>
+    <Container>
+      <Row>
+        <Form>
+          <Form.Group controlId="formFileLg" className="mb-3">
+            <Form.Label>Upload lorebook</Form.Label>
+            <Form.Control type="file" size="lg" onChange={handleUpload} />
+          </Form.Group>
+        </Form>
+      </Row>
+      <Row>{lorebook && <LorebookPanel lorebook={lorebook} />}</Row>
+    </Container>
   );
 };
 
